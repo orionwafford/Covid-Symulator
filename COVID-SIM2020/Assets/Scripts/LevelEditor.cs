aba4a38736f21304f62806d6bf52874b59f26c80
RoboDogPete <61554702+RoboDogPete@users.noreply.github.com>
2021-04-22 10:12:02 -0400

    added level editor script
    script for the level editor

---
+++ COVID-SIM2020/Assets/Scripts/LevelEditor.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEditor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Erase()
+    {
+
+    }
+
+    private void PlaceDoor()
+    {
+
+    }
+
+    private void PlaceRoom()
+    {
+
+    }
+
+    private void LoadMap()
+    {
+
+    }
+
+     private void SaveMap()
+    {
+
+    }
+
+}
